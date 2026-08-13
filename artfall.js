5629a97ce11858be6e8c197266656aa06cc95da7
Violet Renard <violet.renard@proton.me>
2026-08-13 00:55:43 -0400

    feat: log failed download urls
    Co-authored-by: Evelyn Renard
    <hevyclyn@users.noreply.github.com>

--- artfall.js
+++ artfall.js
@@ -1,33 +1,74 @@
-function getCardInfo(tab) {
-    if (!tab.url.includes("scryfall.com/card/")) return null;
+//#region Helpers
 
-    const url_split = tab.url.split("/");
+function getScryfallCardInfo(url) {
+    if (!url.includes("scryfall.com/card/")) return null;
+
+    const url_split = url.split("/");
 
     return {
         set: url_split[4],
-        num: url_split[5].padStart(3, "0"),
-        name: url_split.slice(6).join("-"),
+        num: url_split[5].padStart(3, "0"), // Get full 3-digit collector's num
+        name: url_split.slice(6).join("_"), // Join with `_` because Scryfall
+                                            // joins with `-`
     };
 }
 
-async function handleTab({tab, set, num, name}) {
-    const image_url = `https://www.mtgpics.com/pics/art/${set}/${num}.jpg`;
+function getDownloadCardInfo(url, filename) {
+    if (!url.includes("mtgpics.com/pics/art/")) return null;
+    const url_split = url.split("/");
+    const path_split = filename.split("\\"); // Only on Windows?
 
-    browser.downloads.download({
-            url: image_url,
-            filename: `${name}.jpg`,
-            conflictAction: "overwrite", // Don't create excess copies
-        }).then((result) => {
-            console.log(`Downloaded ${name}.jpg`);
-        }).catch((error) => {
-            console.error(`Couldn't get art for ${name} 3:`);
-            console.error(error);
-        });
+    return {
+        set: url_split[5],
+        num: url_split[6].split(".")[0],
+        name: path_split[path_split.length - 1].split(".")[0], // Remove extension
+    };
+}
+
+function constructScryfallUrl(set, num, name) {
+    if (name.indexOf("_") === -1) {
+        return `https://scryfall.com/card/${set}/${num}/${name}`;
+    } else {
+        const name_split = name.split("_");
+        return `https://scryfall.com/card/${set}/${num}/${name_split[0]}/${name_split[1]}`;
+    }
+}
+
+function constructDownloadUrl(set, num, name) {
+    return `https://www.mtgpics.com/pics/art/${set}/${num}.jpg`;
+}
+
+//#endregion
+//#region Handlers
+
+async function handleInterrupted(delta) {
+    if (delta.state && delta.state.current === "interrupted") {
+        // Reopen the Scryfall page?
+        const item = (await browser.downloads.search({ id: delta.id }))[0];
+
+        console.log(item);
+
+        const cardInfo = getDownloadCardInfo(item.url, item.filename);
+
+        console.log(constructScryfallUrl(cardInfo.set, cardInfo.num, cardInfo.name));
+
+        // const tab = await browser.tabs.create({ url: item.url });
+    }
+}
+
+async function handleTab({tab, set, num, name}) {
+    const downloadUrl = constructDownloadUrl(set, num, name);
+
+    const downloadId = await browser.downloads.download({
+        url: downloadUrl,
+        filename: `${name}.jpg`,
+        conflictAction: "overwrite", // Don't create excess copies yet
+    });
 }
 
 function handleTabs(tabs) {
     for (const tab of tabs) {
-        const cardInfo = getCardInfo(tab);
+        const cardInfo = getScryfallCardInfo(tab.url);
         if (!cardInfo) continue;
 
         handleTab({tab, ...cardInfo})
@@ -37,9 +78,13 @@ function handleTabs(tabs) {
     }
 }
 
+//#endregion
+
 function onError(error) {
     console.error(`Uncaught error: ${error}`);
 }
+
+browser.downloads.onChanged.addListener(handleInterrupted);
 
 browser.action.onClicked.addListener(() => {
     browser.tabs.query({}).then(handleTabs, onError);
