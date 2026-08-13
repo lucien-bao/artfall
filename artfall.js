9c47343a7c5beb5253875cc9324aa20be9667011
Violet Renard <violet.renard@proton.me>
2026-08-12 23:14:26 -0400

    `refactor: extract card info getting and error handling

--- artfall.js
+++ artfall.js
@@ -1,33 +1,40 @@
-async function handleTab(tab) {
-    if (!tab.url.includes("scryfall.com/card/")) return;
+function getCardInfo(tab) {
+    if (!tab.url.includes("scryfall.com/card/")) return null;
 
-    url_split = tab.url.split("/");
+    const url_split = tab.url.split("/");
 
-    set = url_split[4];
-    num = url_split[5].padStart(3, "0");
-    card_name = url_split[url_split.length - 1];
+    return {
+        set: url_split[4],
+        num: url_split[5].padStart(3, "0"),
+        name: url_split.slice(6).join("-"),
+    };
+}
 
-    image_url = `https://www.mtgpics.com/pics/art/${set}/${num}.jpg`;
+async function handleTab({tab, set, num, name}) {
+    const image_url = `https://www.mtgpics.com/pics/art/${set}/${num}.jpg`;
 
-    browser.downloads.download({ url: image_url, filename: `${card_name}.jpg` }).then((result) => {
-        console.log(`Downloaded ${card_name}.jpg`);
+    browser.downloads.download({ url: image_url, filename: `${name}.jpg` }).then((result) => {
+        console.log(`Downloaded ${name}.jpg`);
     }).catch((error) => {
-        console.error(`Couldn't get art for ${tab.url} 3:`);
+        console.error(`Couldn't get art for ${name} 3:`);
         console.error(error);
     });
 }
 
 function handleTabs(tabs) {
     for (const tab of tabs) {
-        handleTab(tab)
+        const cardInfo = getCardInfo(tab);
+        if (!cardInfo) continue;
+
+        handleTab({tab, ...cardInfo})
             .catch((e) => {
-                console.error(`Error getting art for ${tab.url}: ${e}`);
+                console.error(`Error getting art for ${cardInfo.name}: ${e}`);
             });
     }
 }
 
 function onError(error) {
-    console.error(`Error: ${error}`);
+    console.error(`Uncaught error: ${error}`);
 }
 
 browser.action.onClicked.addListener(() => {
